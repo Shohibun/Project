6db7231ab095872c85e5bce26ab0f48dfc0ba195
Shohibun Najam <71695550+Shohibun@users.noreply.github.com>
2020-09-24 10:46:01 +0700

    Delete First.py

--- First.py
+++
@@ -1,9 +0,0 @@
-level = int(input())
-
-for baris in range(level):
-    for kolom in range(level):
-        if baris == kolom or kolom == level-(baris+1) or kolom == 0 or kolom == level-1 or baris == 0 or baris == level-1:
-            print("*", end="")
-        else: 
-            print(" ", end="")
-    print()
